edb772d9a455c78fdb4a54026dc9f54dabf6d5bb
AKRA MESCHEBA Jordy <94695950+akramescheba@users.noreply.github.com>
2023-11-15 00:28:41 +0100

    Modification fonction Drawing, css Body et Footer

--- src/FrontEnd/Body/Body.js
+++ src/FrontEnd/Body/Body.js
@@ -3,12 +3,12 @@ import './Body.css'
 
 
 export default function Body() {
-  const canvasRef1 = useRef(null)
+  const canvasRef = useRef(null)
 
   /* UseEffect pour la gestion de l'événement de dessin */
   useEffect(() => {
-    const canvas1 = canvasRef1.current
-    const context1 = canvas1.getContext('2d')
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
     let isDrawing = false
     function startDrawing(e) {
       isDrawing = true
@@ -17,48 +17,47 @@ export default function Body() {
 
     function stopDrawing() {
       isDrawing = false
-      context1.beginPath()
+      context.beginPath()
     }
 
     function draw(e) {
       if (!isDrawing) return
 
-      const mouseX = e.clientX - context1.canvas.offsetLeft
-      const mouseY = e.clientY - context1.canvas.offsetTop
+      const mouseX = e.clientX - context.canvas.offsetLeft
+      const mouseY = e.clientY - context.canvas.offsetTop
+      /*Taille, contour, et de la couleur du stylo */
+      context.lineWidth = 30
+      context.lineCap = 'round'
+      context.strokeStyle = '#000'
 
-      context1.lineWidth = 5
-      context1.lineCap = 'round'
-      /*style de la couleur du stylo */
-      context1.strokeStyle = '#000'
-
-      context1.lineTo(mouseX, mouseY)
-      context1.stroke()
-      context1.beginPath()
-      context1.moveTo(mouseX, mouseY)
+      context.lineTo(mouseX, mouseY)
+      context.stroke()
+      context.beginPath()
+      context.moveTo(mouseX, mouseY)
     }
 
     function handleKeyDown(e) {
       /* Conditions permettant de supprimer le manuscrit avec CTRL+Z */
       if (e.ctrlKey && e.key === 'z') {
-        context1.clearRect(0, 0, canvas1.width, canvas1.height)
+        context.clearRect(0, 0, canvas.width, canvas.height)
       }
     }
 
-    canvas1.addEventListener('mousedown', startDrawing)
-    canvas1.addEventListener('mouseup', stopDrawing)
-    canvas1.addEventListener('mousemove', draw)
+    canvas.addEventListener('mousedown', startDrawing)
+    canvas.addEventListener('mouseup', stopDrawing)
+    canvas.addEventListener('mousemove', draw)
 
     window.addEventListener('keydown', handleKeyDown)
 
     return () => {
       /*  Nettoie les écouteurs d'événements lorsqu'on démonte le composant*/
-      canvas1.removeEventListener('mousedown', startDrawing)
-      canvas1.removeEventListener('mouseup', stopDrawing)
-      canvas1.removeEventListener('mousemove', draw)
+      canvas.removeEventListener('mousedown', startDrawing)
+      canvas.removeEventListener('mouseup', stopDrawing)
+      canvas.removeEventListener('mousemove', draw)
 
       window.removeEventListener('keydown', handleKeyDown)
     }
-  }, [canvasRef1])
+  }, [canvasRef])
 
   return (
     <div>
@@ -71,7 +70,7 @@ export default function Body() {
                 className="manuscrit-canvas"
                 width={500}
                 height={300}
-                ref={canvasRef1}
+                ref={canvasRef}
               ></canvas>
             </div>
             <div className="manuscrit-column">
