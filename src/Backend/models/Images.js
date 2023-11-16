f82b5d4180a19c16e201c10fcbee40dd5227b938
Eugénie <eugenie.khayat@gmail.com>
2023-11-16 18:19:51 +0100

    build schema model for images

--- src/Backend/models/Images.js
+++ src/Backend/models/Images.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
-const thingSchema = mongoose.Schema({
-  image: { type: String, required: false },
-  description: { type: String, required: false },
+const Schema = mongoose.Schema;
+
+const thingSchema = new Schema({
+  pixels: {
+    type: [Number],
+    required: true,
+  },
+  prediction: {
+    type: Number,
+    required: false,
+  },
 });
 
 module.exports = mongoose.model("Images", thingSchema);
